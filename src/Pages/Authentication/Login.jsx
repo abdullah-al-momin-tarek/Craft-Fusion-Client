cc208304aa48493453a1c4fa608f39e944e06439
Shikder-Tarek-15 <shikdertarek2019@gmail.com>
2024-09-22 19:04:48 +0600

    Add login component

---
+++ src/Pages/Authentication/Login.jsx
@@ -0,0 +1,114 @@
+import { useState } from "react";
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { FaEye, FaEyeSlash, FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [show, setShow] = useState(true);
+  // Navigate
+  const navigate = useNavigate();
+  const location = useLocation();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleLogin = (data) => {};
+
+  const handleGoogle = () => {};
+  const handleGithub = () => {};
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Helmet>
+        <title>CraftFusion | Login</title>
+      </Helmet>
+      <div className="w-full max-w-md p-8 space-y-3 rounded-xl shadow-md shadow-blue-600">
+        <h1 className="text-2xl font-bold text-center">Login</h1>
+        <form
+          noValidate=""
+          action=""
+          className="space-y-6"
+          onSubmit={handleSubmit(handleLogin)}
+        >
+          <div className="space-y-1 text-sm">
+            <label htmlFor="username" className="block dark:text-gray-600">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              {...register("email", { required: true })}
+              className="w-full px-4 py-3 rounded-md dark:border-gray-300 dark:bg-gray-100 dark:text-gray-800 focus:dark:border-violet-600"
+            />
+            {errors.email && <p className="text-red-600">Email is required.</p>}
+          </div>
+          <div className="space-y-1 text-sm relative">
+            <label htmlFor="password" className="block dark:text-gray-600">
+              Password
+            </label>
+            <input
+              type={show ? "text" : "password"}
+              name="password"
+              placeholder="Password"
+              {...register("password", { required: true })}
+              className="w-full px-4 py-3 rounded-md dark:border-gray-300 dark:bg-gray-100 dark:text-gray-800 focus:dark:border-violet-600"
+            />
+            {errors.password && (
+              <p className="text-red-600">Password is required.</p>
+            )}
+            <div className="flex justify-end text-xs dark:text-gray-600">
+              <a rel="noopener noreferrer" href="#">
+                Forgot Password?
+              </a>
+            </div>
+            <span
+              onClick={() => setShow(!show)}
+              className="absolute right-2 top-8 text-xl hover:cursor-pointer"
+            >
+              {show ? <FaEyeSlash /> : <FaEye />}
+            </span>
+          </div>
+          <button className="block w-full p-3 text-center rounded-sm dark:text-gray-50 bg-orange-500">
+            Sign in
+          </button>
+        </form>
+        <div className="flex items-center pt-4 space-x-1">
+          <div className="flex-1 h-px sm:w-16 dark:bg-gray-300"></div>
+          <p className="px-3 text-sm dark:text-gray-600">
+            Login with social accounts
+          </p>
+          <div className="flex-1 h-px sm:w-16 dark:bg-gray-300"></div>
+        </div>
+        <div className="flex justify-center space-x-">
+          <button
+            onClick={handleGoogle}
+            aria-label="Log in with Google"
+            className="p-3 rounded-sm text-3xl"
+          >
+            <FcGoogle />
+          </button>
+          <button
+            onClick={handleGithub}
+            aria-label="Log in with GitHub"
+            className="p-3 rounded-sm text-3xl"
+          >
+            <FaGithub />
+          </button>
+        </div>
+        <p className="text-xs text-center sm:px-6 dark:text-gray-600">
+          Do not have an account?
+          <Link className="link-primary" to="/register">
+            Register
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,9 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Login from "../Pages/Authentication/Login";
+import Home from "../Pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
   },
 ]);
 
