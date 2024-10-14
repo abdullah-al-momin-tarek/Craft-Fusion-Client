b0e5db023616dbeba1a5995c6802eb4be8d3b2d7
Shikder Tarek <shikdertarek2019@gmail.com>
2024-10-14 23:29:36 +0600

    Test

--- src/Pages/Authentication/Register.jsx
+++ src/Pages/Authentication/Register.jsx
@@ -201,7 +201,7 @@ const Register = () => {
         <div className="flex items-center pt-4 space-x-1">
           <div className="flex-1 h-px sm:w-16 dark:bg-gray-300"></div>
           <p className="px-3 text-sm dark:text-gray-600">
-            Login with social accounts
+            Login with social accounts 
           </p>
           <div className="flex-1 h-px sm:w-16 dark:bg-gray-300"></div>
         </div>
