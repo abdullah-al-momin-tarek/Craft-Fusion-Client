35d755c2ef6183436434016613d50a9bb1dc63de
Shikder Tarek <shikdertarek2019@gmail.com>
2024-10-21 23:30:55 +0600

    Fix: comment

--- src/Components/Dashboard/User/MyProducts.jsx
+++ src/Components/Dashboard/User/MyProducts.jsx
@@ -163,7 +163,6 @@ const MyProducts = () => {
 <dialog id="add-product" className="modal">
   <div className="modal-box">
     <form method="dialog">
-      {/* if there is a button in form, it will close the modal */}
       <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
     </form>
     <form onSubmit={handleSubmit(onSubmit)}>
